e068fb0fe0377de60579d24adaedd885f73883da
Kartikduttasap <kartik.dutta.sap@gmail.com>
2022-11-28 18:19:23 +0530

    First Commit

---
+++ webapp/test/flpSandbox.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+	"sap/base/util/ObjectPath",
+	"sap/ushell/services/Container"
+], function (ObjectPath) {
+	"use strict";
+
+	// define ushell config
+	ObjectPath.set(["sap-ushell-config"], {
+		defaultRenderer: "fiori2",
+		bootstrapPlugins: {
+			"RuntimeAuthoringPlugin": {
+				component: "sap.ushell.plugins.rta",
+				config: {
+					validateAppVersion: false
+				}
+			},
+			"PersonalizePlugin": {
+				component: "sap.ushell.plugins.rta-personalize",
+				config: {
+					validateAppVersion: false
+				}
+			}
+		},
+		renderers: {
+			fiori2: {
+				componentData: {
+					config: {
+						enableSearch: false,
+						rootIntent: "Shell-home"
+					}
+				}
+			}
+		},
+		services: {
+			"LaunchPage": {
+				"adapter": {
+					"config": {
+						"groups": [{
+							"tiles": [{
+								"tileType": "sap.ushell.ui.tile.StaticTile",
+								"properties": {
+									"title": "CRUD Operations",
+									"targetURL": "#nscrud-display"
+								}
+							}]
+						}]
+					}
+				}
+			},
+			"ClientSideTargetResolution": {
+				"adapter": {
+					"config": {
+						"inbounds": {
+							"nscrud-display": {
+								"semanticObject": "nscrud",
+								"action": "display",
+								"description": "CRUD Operations",
+								"title": "CRUD Operations",
+								"signature": {
+									"parameters": {}
+								},
+								"resolutionResult": {
+									"applicationType": "SAPUI5",
+									"additionalInformation": "SAPUI5.Component=ns.crud",
+									"url": sap.ui.require.toUrl("ns/crud")
+								}
+							}
+						}
+					}
+				}
+			},
+			NavTargetResolution: {
+				config: {
+					"enableClientSideTargetResolution": true
+				}
+			}
+		}
+	});
+
+	var oFlpSandbox = {
+		init: function () {
+			/**
+			 * Initializes the FLP sandbox
+			 * @returns {Promise} a promise that is resolved when the sandbox bootstrap has finshed
+			 */
+
+			// sandbox is a singleton, so we can start it only once
+			if (!this._oBootstrapFinished) {
+				this._oBootstrapFinished = sap.ushell.bootstrap("local");
+				this._oBootstrapFinished.then(function () {
+					sap.ushell.Container.createRenderer().placeAt("content");
+				});
+			}
+
+			return this._oBootstrapFinished;
+		}
+	};
+
+	return oFlpSandbox;
+});
